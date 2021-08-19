839fd7019424f3223ae5c835a5ccf29b334a47cf
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2021-08-19 13:52:46 +0800

    rename

---
+++ day06/lib/demo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DemoCenter extends StatelessWidget {
+  const DemoCenter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
