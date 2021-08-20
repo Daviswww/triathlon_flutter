2125f892c61d5a80d0d44641e8b0f7ba56822c22
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2021-08-20 18:45:11 +0800

    day9

--- day09/lib/demo.dart
+++ day09/lib/demo.dart
@@ -1,12 +1,51 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class DemoButton extends StatelessWidget {
-  const DemoButton({Key? key}) : super(key: key);
+class DemoOutlinedButton extends StatelessWidget {
+  const DemoOutlinedButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Hello World"),
+      child: OutlinedButton(
+        child: Text("Hello"),
+        onPressed: () {
+          log("click", name: "INFO");
+        },
+      ),
+    );
+  }
+}
+
+class DemoTextButton extends StatelessWidget {
+  const DemoTextButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: OutlinedButton(
+        child: Text("Hello"),
+        onPressed: () {
+          log("click", name: "INFO");
+        },
+      ),
+    );
+  }
+}
+
+class DemoIconButton extends StatelessWidget {
+  const DemoIconButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: IconButton(
+        icon: Icon(Icons.ac_unit),
+        onPressed: () {
+          log("click", name: "INFO");
+        },
+      ),
     );
   }
 }
---
+++ day10/android/app/src/main/kotlin/com/example/day10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
