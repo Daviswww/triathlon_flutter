5386f1fcf18157dbc5d584f66c42d9343a19b252
Hsing-Wei Ho <hwho@datalab.cs.nthu.edu.tw>
2021-09-02 15:21:22 +0800

    day11

---
+++ day11/lib/demo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoStatful extends StatefulWidget {
+  const DemoStatful({Key? key}) : super(key: key);
+
+  @override
+  _DemoStatfulState createState() => _DemoStatfulState();
+}
+
+class _DemoStatfulState extends State<DemoStatful> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
